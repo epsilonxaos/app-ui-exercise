cb830ae465d78cc65d5ea7c28f155899eab4ad51
Epsilonxaos <angelotr12@hotmail.com>
2024-09-03 15:58:13 -0600

    feat(App): :sparkles: Agregando prettier y eslint

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, TouchableHighlight, View } from "react-native";
+
+const logo = require("./assets/img/logo.jpg");
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Primeros pasos</Text>
-			<StatusBar style="auto" />
+			<StatusBar style="dark" />
+
+			<Image fadeDuration={600} source={logo} style={{ width: 350, marginBottom: 30, resizeMode: "center" }} />
+			<Text style={{ marginBottom: 30 }}>Primeros pasos</Text>
+
+			<TouchableHighlight
+				onPress={() => {
+					alert("🍌 Banana");
+				}}
+				underlayColor={"#84a98c"}
+				style={{
+					borderRadius: 5,
+					backgroundColor: "#b0c4b1",
+					paddingTop: 10,
+					paddingBottom: 10,
+					paddingLeft: 30,
+					paddingRight: 30,
+					marginBottom: 20,
+				}}
+			>
+				<Text>Pulsa Aqui</Text>
+			</TouchableHighlight>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		padding: "60 20",
 		flex: 1,
-		backgroundColor: "#fff",
+		backgroundColor: "#eaeaea",
 		alignItems: "center",
 		justifyContent: "center",
 	},
